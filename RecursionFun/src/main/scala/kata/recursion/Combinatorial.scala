974e47ce9a40960fc7fc7a1b55a52f92ca88142a
Marcio Gualtieri <marcio.gualtieri@gmail.com>
2016-11-11 21:42:25 +0000

    Improved combinatorial

--- RecursionFun/src/main/scala/kata/recursion/Combinatorial.scala
+++ RecursionFun/src/main/scala/kata/recursion/Combinatorial.scala
@@ -2,10 +2,17 @@ package kata.recursion
 
 class Combinatorial {
 
-  def choose[T](k: Int, elems: Set[T]): Set[Set[T]] = {
+  def choose[T](elems: Set[T], k: Int): Set[Set[T]] = {
     if (k > elems.size) Set()
     else if (k == 1) elems.map(Set(_))
-    else choose(k - 1, elems.tail).map(_ + elems.head) ++ choose(k, elems.tail)
+    else choose(elems.tail, k - 1).map(_ + elems.head) ++ choose(elems.tail, k)
+  }
+
+  def choose[T](n: Int, k: Int): Int = {
+    println(s"n: [$n] k: [$k]")
+    if (k > n) 0
+    else if (k == 1) n
+    else choose(n - 1, k - 1) + choose(n - 1, k)
   }
 }
 
--- RecursionFun/src/test/scala/kata/recursion/CombinatorialTest.scala
+++ RecursionFun/src/test/scala/kata/recursion/CombinatorialTest.scala
@@ -5,14 +5,16 @@ import org.scalatest._
 class CombinatorialTest extends FlatSpec with Matchers with BeforeAndAfter {
 
   "3 choose 2" should "be return the correspondent combinations." in {
-    Combinatorial().choose(2, Set(1, 2, 3)) shouldBe Set(Set(1, 2), Set(1, 3),
+    Combinatorial().choose(Set(1, 2, 3), 2) shouldBe Set(Set(1, 2), Set(1, 3),
                                                          Set(2, 3))
+    Combinatorial().choose(3, 2) shouldBe 3
   }
 
   "4 choose 2" should "be return the correspondent combinations." in {
-    Combinatorial().choose(2, Set(1, 2, 3, 4)) shouldBe Set(Set(1, 2), Set(1, 3), Set(1, 4),
+    Combinatorial().choose(Set(1, 2, 3, 4), 2) shouldBe Set(Set(1, 2), Set(1, 3), Set(1, 4),
                                                             Set(2, 3), Set(2, 4),
                                                             Set(3, 4))
+    Combinatorial().choose(4, 2) shouldBe 6
   }
 
 }
